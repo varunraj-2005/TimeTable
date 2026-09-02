e0eae8ebe0d66a193c735c85145b84fe9cd183bf
varunraj-2005 <rajapandian1412@gmail.com>
2026-09-02 20:47:09 +0530

    The backend file Added

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
